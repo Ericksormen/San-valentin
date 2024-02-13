d1942dd47bf00e2d4828c33c837c5abeb6f10412
Ericksormen <156144037+Ericksormen@users.noreply.github.com>
2024-02-12 21:03:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("no-btn").addEventListener("click", function() {
   document.getElementById("si-btn").style.fontSize = "24px"; // Cambiar tamaño del botón "Sí"
-  document.getElementById("imagen").src = "https://imgur.com/oAefGJ8"; // Cambiar la URL de la imagen cuando la respuesta es "No"
+  document.getElementById("imagen").src = "https://giphy.com/gifs/FEyLVDNqrgowE0uJhX"; // Cambiar la URL de la imagen cuando la respuesta es "No"
 });
 
 document.getElementById("si-btn").addEventListener("click", function() {
-  document.getElementById("imagen").src = "https://imgur.com/xFK91Ex"; // Cambiar la URL de la imagen cuando la respuesta es "Sí"
+  document.getElementById("imagen").src = "https://giphy.com/gifs/tip3RkaWaqvpdbzc9E"; // Cambiar la URL de la imagen cuando la respuesta es "Sí"
 });
